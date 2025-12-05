a9f98cb23d0513b6375bed56b69d8c8a24c6b9c6
Alexander Emerson Batalha Carlos <133721294+Nevrand@users.noreply.github.com>
2025-12-04 21:02:01 -0400

    Extractor.py está funcionando com o pdfplumber e extraindo o exemplo.pdf

--- src/pdf/extractor.py
+++ src/pdf/extractor.py
@@ -1,10 +1,14 @@
-
+# Importando o pdfplumber como nossa ferramenta principal para extração de texto
 import pdfplumber
 
-pdf_path = 'exemplo.pdf'
+# Caminho do arquivo PDF a ser extraído
+pdf_path = 'exemplos_pdf/exemplo.pdf'
 
+# Extração de texto do PDF
 texto = ''
 with pdfplumber.open(pdf_path) as pdf:
+    # Percorre todas as páginas do PDF
     for page in pdf.pages:
-        texto = page.extract_text()
-        print(texto)
+        texto += page.extract_text()
+        
+print(texto)
