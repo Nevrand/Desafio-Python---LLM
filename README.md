321be643b1eacd5a51e997d75bba57e4b2c4a3d0
Alexander Emerson Batalha Carlos <133721294+Nevrand@users.noreply.github.com>
2025-12-05 15:53:11 -0300

    Adicionando o modelo phi-3.5-mini-instruct como modelo no extractor.py

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Desafio em Python para avaliação de capacidade de organizar um projeto em Pyth
 
 Instalar pdfpulmber com py -m pip install pdfplumber ou pip install pdfplumber
 Rodar com o py ./src/pdf/extractor.py
+
+Instalar pip install torch ou py -m pip install torch
+Instalar pip install transformers ou py -m pip install transformers
+Usar o microsoft/phi-3.5-mini-instruct
--- src/pdf/extractor.py
+++ src/pdf/extractor.py
@@ -1,7 +1,9 @@
-# Importando o pdfplumber como ferramenta principal para extração de texto
+# Importando o pdfplumber como ferramenta principal para extração de texto como torch e transformers
 import pdfplumber
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# Caminho do arquivo PDF a ser extraído
+# Caminho do arquivo PDF a ser extraído de exemplo para testes
 pdf_path = 'exemplos_pdf/exemplo.pdf'
 
 # Extração de texto do PDF
@@ -10,8 +12,28 @@ with pdfplumber.open(pdf_path) as pdf:
     # Percorre todas as páginas do PDF
     for page in pdf.pages:
         texto += page.extract_text()
-        
+
+# Modelo que irá ser utilizado para o projeto
+modelo = "microsoft/phi-3.5-mini-instruct"
+
+# Carregando o modelo phi-mini-instruct
+tokenizer = AutoTokenizer.from_pretrained(modelo)
+model = AutoModelForCausalLM.from_pretrained(modelo, torch_dtype=torch.float16, device_map="auto")
+
+# Prompt para ser usado no phi-mini-instruct
+prompt = f"Extraia as informações do seguinte texto:\n\n{texto}\n\n"
+
+# Entrada do modelo
+entrada = tokenizer(prompt, return_tensors= "pt", truncation = True)
+
+# Saída do modelo
+saida = model.generate(**entrada, max_new_tokens=512, temperature=0)
+
+# Decodificando a saída do modelo
+resultado = tokenizer.decode(saida[0], skip_special_tokens=True)
+
 print(texto)
 print("Número total de páginas extraídas:", len(pdf.pages))
 print("Número total de palavras extraídas:", len(texto.split()))
 print("Tamanho em bytes do texto extraído:", len(texto.encode('utf-8')))
+print("Resultado do modelo phi-mini-instruct:", resultado)
