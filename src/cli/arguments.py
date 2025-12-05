5568bea0f343cfb47409b2ca0775d0c559e6eba0
Alexander Emerson Batalha Carlos <133721294+Nevrand@users.noreply.github.com>
2025-12-04 20:36:24 -0400

    Fazendo mudanças no extractor.py

---
+++ src/cli/arguments.py
@@ -0,0 +1,6 @@
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="pdf_extração",
+    description="Extração de PDF para leitura" )
--- src/pdf/extractor.py
+++ src/pdf/extractor.py
@@ -1,1 +1,10 @@
-# Comentário
+
+import pdfplumber
+
+pdf_path = 'exemplo.pdf'
+
+texto = ''
+with pdfplumber.open(pdf_path) as pdf:
+    for page in pdf.pages:
+        texto = page.extract_text()
+        print(texto)
